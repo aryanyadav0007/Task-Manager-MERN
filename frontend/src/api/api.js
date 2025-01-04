abde6074d1d4ab3a71b655b117f1082cd24b19dd
aryanyadav0007 <164308239+aryanyadav0007@users.noreply.github.com>
2025-01-04 12:19:05 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({ baseURL: "https://task-manager-mern-tryd.onrender.com/api" });
 
 // Intercept requests to add token
 API.interceptors.request.use((req) => {
